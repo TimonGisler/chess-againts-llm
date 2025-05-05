ed2f52f117683eee7d7cfa89bc8986f57ca2d69c
Timon Gisler <timon.gisler@students.fhnw.ch>
2025-05-05 15:22:09 +0200

    made size etc a little bit better

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,8 @@ import { Chessboard } from "react-chessboard";
 function App() {
 
   return (
-      <div style={ {height: "100vh", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center"} }>
-        
-        <h1 className='text-red-300'>TEST</h1>
-        <Chessboard id="BasicBoard" />
+      <div className='bg-red-800 w-lg h-lg'>
+        <Chessboard></Chessboard>
       </div>
   )
 }
