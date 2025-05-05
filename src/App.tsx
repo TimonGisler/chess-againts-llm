8cffdcbdefed8891944bc514e7616aa81f284a77
Timon Gisler <timon.gisler@students.fhnw.ch>
2025-05-05 14:57:38 +0200

    installed tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,9 @@ import { Chessboard } from "react-chessboard";
 function App() {
 
   return (
-      <div>
-        TEST
+      <div style={ {height: "100vh", width: "100vw", display: "flex", justifyContent: "center", alignItems: "center"} }>
+        
+        <h1 className='text-red-300'>TEST</h1>
         <Chessboard id="BasicBoard" />
       </div>
   )
