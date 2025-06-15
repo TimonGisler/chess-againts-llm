8cf760e9364e39844e8ab87f8b6afeca0bce4446
timon <timongisler@outlook.com>
2025-06-15 17:39:18 +0200

    added mui for components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { Chessboard } from "react-chessboard";
 import { Chess } from "chess.js";
 import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import { Grid, Paper, styled } from "@mui/material";
 
 function App() {
   const [game, setGame] = useState(new Chess());
@@ -10,6 +12,17 @@ function App() {
   const [llmPrompt, setLlmPrompt] = useState<string>(
     "Lets play a chess game, i will provide you with my move e.g. e4 and you will answer with your move. Your move must only be a chess notation e.g. e5, and nothing else, not other text"
   );
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: (theme.vars ?? theme).palette.text.secondary,
+    ...theme.applyStyles("dark", {
+      backgroundColor: "#1A2027",
+    }),
+  }));
 
   function handleClick() {
     console.log("TEST MAKING SOME MOVE the move is: ", moveInput);
@@ -25,29 +38,41 @@ function App() {
   }
 
   return (
-    <div className="flex">
-      <div id="chessboard" className="w-lg h-lg flex-1">
-        <Chessboard position={game.fen()}></Chessboard>
-      </div>
-
-      <div id="llm-chat" className="flex-1">
-        <textarea
-          placeholder="type your move here"
-          value={llmPrompt}
-          onChange={(e) => setLlmPrompt(e.target.value)}
-        ></textarea>
-        <input
-          type="text"
-          placeholder="type your move here"
-          value={moveInput}
-          onChange={(e) => setMoveInput(e.target.value)}
-        />
-        <button onClick={handleClick} className="rounded-2xl bg-amber-800">
+    <Grid container spacing={2}>
+      <Grid size={8}>
+        <Item>
+          <div id="chessboard" className="w-lg h-lg flex-1">
+            <Chessboard position={game.fen()}></Chessboard>
+          </div>
+        </Item>
+      </Grid>
+      <Grid size={4}>
+        <Item>
           {" "}
-          Test ask{" "}
-        </button>
-      </div>
-    </div>
+          <div id="llm-chat" className="flex-1">
+            <TextField
+              id="outlined-multiline-static"
+              label="Multiline"
+              multiline
+              rows={4}
+              defaultValue="type your llm prompt here"
+              value={llmPrompt}
+              onChange={(e) => setLlmPrompt(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="type your move here"
+              value={moveInput}
+              onChange={(e) => setMoveInput(e.target.value)}
+            />
+            <button onClick={handleClick} className="rounded-2xl bg-amber-800">
+              {" "}
+              Test ask{" "}
+            </button>
+          </div>
+        </Item>
+      </Grid>
+    </Grid>
   );
 }
 
